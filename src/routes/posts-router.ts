42b181358ab3d39361e31081a28a855131cfb7ca
Bruna Terribile <brunaterribile@gmail.com>
2023-06-22 10:39:32 -0300

    fix: rename my profile routes

--- src/routes/posts-router.ts
+++ src/routes/posts-router.ts
@@ -1,17 +1,17 @@
-import { deletePost, getAllPosts, getPostById, getUserPosts } from "@/controllers/posts-controller.js";
+import { createPost, deletePost, getAllPosts, getPostById, getUserPosts } from "../controllers/posts-controller.js";
 import { authenticateToken } from "../middlewares/authentication-middleware.js";
 import { Router } from "express";
 
 const postsRouter = Router();
 
 postsRouter
-//    .all('/*', authenticateToken)
+    .all('/*', authenticateToken)
     .get('/', getAllPosts)
     .get('/:id', getPostById)
-    .get('/myposts', getUserPosts)
-    .post('/myposts', createPost)
-    .delete('/myposts/delete/:id', deletePost)
-//.update('/myposts/update/:id', updatePost)
+    .get('/my-posts', getUserPosts)
+    .post('/my-posts', createPost)
+    .delete('/my-posts/delete/:id', deletePost)
+//.update('/my-posts/update/:id', updatePost)
 
 
 export { postsRouter }
